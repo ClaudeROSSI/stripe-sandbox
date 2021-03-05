0dd77e803b99a83fa5c2c95878b80fb933e74af3
Claude ROSSI <claude.rossi@sap.com>
2021-03-05 17:35:28 +0100

    stripe investigation

---
+++ lc.js
@@ -0,0 +1,22 @@
+const env = require("dotenv").config({ path: "./.env" });
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+(async () => {
+  console.log('Hello!');
+  const customers = await stripe.customers.list({
+    limit: 1,
+    email: 'claude.rossi@sap.com'
+  });
+  console.log(customers);
+  const paymentMethods = await stripe.paymentMethods.list({
+    customer: customers.data[0].id,
+    type: 'card',
+  });
+  console.log('Payment Methods .....'); 
+  console.log(paymentMethods);
+  
+  console.log('CARDS Information ......................');
+  paymentMethods.data.forEach(paymentMethod => {
+    console.log("Card: ................................");
+    console.log(paymentMethod.card);
+  });
+})();
